a120dbd7cc8b851ca3fc9ae881925ccf9709d53b
OBAME Gad Erwin <116199106+Bisso2022@users.noreply.github.com>
2022-12-01 11:35:05 +0100

    C:\Users\Fenris Agency\fyyur-project\venv

---
+++ config.py
@@ -0,0 +1,20 @@
+import os
+SECRET_KEY = os.urandom(32)
+# Grabs the folder where the script runs.
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+# Enable debug mode.
+DEBUG = True
+
+# Connect to the database
+
+
+# TODO IMPLEMENT DATABASE URL
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:will@localhost:5432/datafyyur'
+# Remove console warning
+SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+
+
+
+
